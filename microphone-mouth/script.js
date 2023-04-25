9bdb9a4875aaaafa06eb4b7072bfca2c6baee25a
abel.silva <abel.tdsilva@gmail.com>
2023-04-25 10:26:21 +0100

    ADD - cheat code for mic to work on load

--- microphone-mouth/script.js
+++ microphone-mouth/script.js
@@ -1,18 +1,34 @@
-let audioIn;
+let audioIn,
+    audioPlaying = false;
 
 function setup() {
-    createCanvas(400, 400);
+    let canvas = createCanvas(400, 400);
     audioIn = new p5.AudioIn();
-    audioIn.start();
+    canvas.mousePressed(audioToggle)
 }
+
+// FOR MIC TO WORK ON LOAD
+function touchStarted() { getAudioContext().resume(); } 
 
 function draw() {
     let volume = audioIn.getLevel();
     console.log(volume);
     background(155);
     ellipse(width / 2, height - 100, width - 200, volume * 2000);
+    text('Touch me to Start', width / 2, height - 20)
+    textAlign(CENTER)
     
     // eyes!
     ellipse(width / 4, height / 4, 50, 50);
     ellipse(width * .75, height / 4, 50, 50);
+}
+
+let audioToggle = () => {
+    if (audioPlaying) {
+        audioIn.stop();
+    } else {
+        audioIn.start();
+    }
+
+    audioPlaying = !audioPlaying
 }
